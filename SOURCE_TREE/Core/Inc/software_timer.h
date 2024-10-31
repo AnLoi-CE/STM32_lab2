eb3c6ec90b104ff780a619646252a9edc2ae410f
loinguyen1412ktmt <loi.nguyen1412ktmt@hcmut.edu.vn>
2024-10-31 08:49:08 +0700

    Ex2-extra

--- SOURCE_TREE/Core/Inc/software_timer.h
+++ SOURCE_TREE/Core/Inc/software_timer.h
@@ -11,10 +11,13 @@
 #include "main.h"
 extern int timer1_flag;
 extern int timer2_flag;
+extern int led_buffer[4] = {1 , 2 , 3 , 0};
 
 void setTimer1(int duration);
 void setTimer2(int duration);
 void timerRun();
 void display7SEG(int num);
+void update7SEG(int index);
+
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
